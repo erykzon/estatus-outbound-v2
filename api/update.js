2aa6454da5d6b094933ad8ef5b6b49d25d19f31a
Erykzon <69286881+erykzon@users.noreply.github.com>
2026-07-21 14:32:50 -0600

    Update update.js
    test de info ejemplo

--- api/update.js
+++ api/update.js
@@ -1,1 +1,27 @@
+export default function handler(req, res) {
 
+    if (req.method !== "POST") {
+
+        return res.status(405).json({
+
+            success: false,
+
+            message: "Method Not Allowed"
+
+        });
+
+    }
+
+    const body = req.body;
+
+    return res.status(200).json({
+
+        success: true,
+
+        received: body,
+
+        message: "Datos recibidos correctamente"
+
+    });
+
+}
