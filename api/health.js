b64f69f7296e376d68e6798fc98ed74510749411
Erykzon <69286881+erykzon@users.noreply.github.com>
2026-07-21 14:31:47 -0600

    Update health.js
    actualziando lineas de healt

--- api/health.js
+++ api/health.js
@@ -1,1 +1,15 @@
+export default function handler(req, res) {
 
+    res.status(200).json({
+
+        status: "OK",
+
+        project: "Estatus Outbound V2",
+
+        version: "1.0",
+
+        serverTime: new Date().toISOString()
+
+    });
+
+}
