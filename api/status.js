68d46ac6469f1bf013d7591e8edcc44e9cabfc60
Erykzon <69286881+erykzon@users.noreply.github.com>
2026-07-21 14:32:20 -0600

    Update status.js
    info testing

--- api/status.js
+++ api/status.js
@@ -1,1 +1,15 @@
+export default function handler(req, res) {
 
+    res.status(200).json({
+
+        success: true,
+
+        history: 0,
+
+        rows: [],
+
+        lastUpdate: null
+
+    });
+
+}
